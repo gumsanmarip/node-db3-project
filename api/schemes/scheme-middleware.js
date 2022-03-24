39c1e6f309d50282860a9cafb16d540477f67081
Gumsan <maripga@vcu.edu>
2022-03-23 22:09:37 -0400

    start

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -19,7 +19,15 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
+  const { instructions } = req.body.instructions;
+  const { stepNum } = req.body.step_number;
+  if(!instructions || instructions === '' || typeof instructions !== "string" || typeof stepNum !== "number" || stepNum < 1) {
+    res.status(400).json({
+      "message": "invalid scheme_name"
+    })
+  } else {
+    next();
+  }
 }
 
 /*
